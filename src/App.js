89dd5c4a7835d2ec2d0508a75638f2cc8d4fe872
baileyg2016 <bspell20@vt.edu>
2020-04-13 10:50:23 -0400

    changed a line of html

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <div>
-          <h1>Get back to work! Don't let Pacman eat all your time!</h1>
+          <h1>Get back to work! You are supposed to be changing the world.</h1>
         </div>
         <div style={{margin: "0", position: "absolute", left: "25%"}}>
           <img src={kid} alt={'get back to work'} />
